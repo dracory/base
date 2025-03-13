3077709a86b8a615c01f8401ec27a809264f0a7e
lesichkovm <lesichkovm@gmail.com>
2025-03-13 03:09:13 +0000

    added docs

--- env/bool.go
+++ env/bool.go
@@ -4,18 +4,11 @@ import (
 	"strconv"
 )
 
-func getEnvBool(key string, defaultValue bool) bool {
-	valueStr := Value(key)
-
-	value, err := strconv.ParseBool(valueStr)
-	if err != nil {
-		return defaultValue
-	}
-
-	return value
-}
-
 // Bool returns the value for an environment key
+//
+// Any of the following values are considered true: "true", "True", "TRUE", "T", "t", "1"
+// Any of the following values are considered false: "false", "False", "FALSE", "F", "f", "0"
+// Any other value returns the default false.
 //
 // Parameters:
 //   - key: The environment key
@@ -28,6 +21,10 @@ func Bool(key string) bool {
 
 // BoolDefault returns the value for an environment key with a default value
 //
+// Any of the following values are considered true: "true", "True", "TRUE", "T", "t", "1"
+// Any of the following values are considered false: "false", "False", "FALSE", "F", "f", "0"
+// Any other value returns the default value.
+//
 // Parameters:
 //   - key: The environment key
 //   - defaultValue: The default value
@@ -36,4 +33,30 @@ func Bool(key string) bool {
 //   - The value for the environment key
 func BoolDefault(key string, defaultValue bool) bool {
 	return getEnvBool(key, defaultValue)
+}
+
+// getEnvBool returns the value for an environment key as a boolean, or the default value if not set
+//
+// Any of the following values are considered true: "true", "True", "TRUE", "T", "t", "1"
+// Any of the following values are considered false: "false", "False", "FALSE", "F", "f", "0"
+// Any other value returns the default value.
+//
+// This function handles base64 and obfuscated prefixes.
+//
+// Parameters:
+//   - key: The environment key
+//   - defaultValue: The default value
+//
+// Returns:
+//   - The value for the environment key
+func getEnvBool(key string, defaultValue bool) bool {
+	valueStr := Value(key)
+
+	value, err := strconv.ParseBool(valueStr)
+	if err != nil {
+		// Return the default value if the value can't be parsed as a boolean
+		return defaultValue
+	}
+
+	return value
 }
