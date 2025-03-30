7e048bb191b960da1cf5bd9b743bbfd1055f8654
lesichkovm <lesichkovm@gmail.com>
2025-03-30 10:07:31 +0100

    added markdown support

---
+++ markdown/markdown_to_html_test.go
@@ -0,0 +1,63 @@
+package markdown
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMarkdownToHtml(t *testing.T) {
+	tests := []struct {
+		name     string
+		markdown string
+		html     string
+	}{
+		{
+			name:     "empty string",
+			markdown: "",
+			html:     "",
+		},
+		{
+			name:     "whitespace only",
+			markdown: "   ",
+			html:     "",
+		},
+		{
+			name:     "plain text",
+			markdown: "Hello World",
+			html:     "<p>Hello World</p>\n",
+		},
+		{
+			name:     "bold text",
+			markdown: "**bold**",
+			html:     "<p><strong>bold</strong></p>\n",
+		},
+		{
+			name:     "italic text",
+			markdown: "*italic*",
+			html:     "<p><em>italic</em></p>\n",
+		},
+		{
+			name:     "heading",
+			markdown: "# Heading",
+			html:     "<h1 id=\"heading\">Heading</h1>\n",
+		},
+		{
+			name:     "unordered list",
+			markdown: "- item1\n- item2",
+			html:     "<ul>\n<li>item1</li>\n<li>item2</li>\n</ul>\n",
+		},
+		{
+			name:     "code block",
+			markdown: "```\ncode\n```",
+			html:     "<pre><code>code\n</code></pre>\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := MarkdownToHtml(tt.markdown)
+			assert.Equal(t, tt.html, result)
+		})
+	}
+}
