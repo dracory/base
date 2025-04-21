d232207c82e95b8b2092f7d8e611d360c85a7df1
lesichkovm <lesichkovm@gmail.com>
2025-04-21 19:35:55 +0100

    enhance getEnvBool function to recognize 'yes' and 'no' as boolean values, and handle empty string case

--- env/bool.go
+++ env/bool.go
@@ -2,6 +2,7 @@ package env
 
 import (
 	"strconv"
+	"strings"
 )
 
 // Bool returns the value for an environment key
@@ -37,8 +38,8 @@ func BoolDefault(key string, defaultValue bool) bool {
 
 // getEnvBool returns the value for an environment key as a boolean, or the default value if not set
 //
-// Any of the following values are considered true: "true", "True", "TRUE", "T", "t", "1"
-// Any of the following values are considered false: "false", "False", "FALSE", "F", "f", "0"
+// Any of the following values are considered true: "true", "True", "TRUE", "T", "t", "1", "yes", "Yes", "YES"
+// Any of the following values are considered false: "false", "False", "FALSE", "F", "f", "0", "no", "No", "NO"
 // Any other value returns the default value.
 //
 // This function handles base64 and obfuscated prefixes.
@@ -51,6 +52,14 @@ func BoolDefault(key string, defaultValue bool) bool {
 //   - The value for the environment key
 func getEnvBool(key string, defaultValue bool) bool {
 	valueStr := Value(key)
+
+	if valueStr == "" {
+		return defaultValue
+	}
+
+	if strings.ToLower(valueStr) == "yes" {
+		return true
+	}
 
 	value, err := strconv.ParseBool(valueStr)
 	if err != nil {
