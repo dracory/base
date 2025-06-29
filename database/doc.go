d0d79248b4acb071e365be0c77bf37d50ce60ef7
lesichkovm <lesichkovm@gmail.com>
2025-06-29 12:05:53 +0100

    feat: database moved to separate module

--- database/doc.go
+++
@@ -1,6 +0,0 @@
-// Package database contains functions to interact with the database.
-//
-// The package is imported like this:
-//
-//	import "github.com/gouniverse/base/database"
-package database
